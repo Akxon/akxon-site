f0298267ca96aeb672667a4cf54f52830a76ce5d
Akxon <jr_bar@hotmail.com>
2025-12-05 22:42:13 -0300

    Delete ucd_app.js

--- ucd_app.js
+++
@@ -1,6 +0,0 @@
-console.log('UCD Frontend iniciado');
-
-document.querySelector('.ucd-login-form')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    window.location.href = 'dashboard.html';
-});
